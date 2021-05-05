48c66f015ebe8325f0770cdc194fc7598070bc37
@anderson1471669814 <anderson_ribeiro.a@hotmail.com>
2021-05-05 15:16:47 -0300

    Projet hr-payroll created

---
+++ hrpay-roll/src/main/java/com/dev/hrpayroll/HrpayRollApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrpayRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrpayRollApplication.class, args);
+	}
+
+}
