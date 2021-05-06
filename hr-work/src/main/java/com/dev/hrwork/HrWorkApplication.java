cd11ae7680311e2530ff666736e3a944c4be45d1
@anderson1471669814 <anderson_ribeiro.a@hotmail.com>
2021-05-06 13:38:02 -0300

    Configuration eureka client

--- hr-work/src/main/java/com/dev/hrwork/HrWorkApplication.java
+++ hr-work/src/main/java/com/dev/hrwork/HrWorkApplication.java
@@ -2,8 +2,10 @@ package com.dev.hrwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrWorkApplication {
 
 	public static void main(String[] args) {
